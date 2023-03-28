d36f01880b596702bd13afd293acf2be70d65de2
nismo <61671297+nismo1337@users.noreply.github.com>
2023-03-28 07:20:16 +0200

    Update robloxchecker.py

--- robloxchecker.py
+++ robloxchecker.py
@@ -1,7 +1,12 @@
-# Simple ROBLOX user info retriever via id.
-# Written by tuny
-import sys
+import os
+import discord
+from discord.ext import commands
 import requests
+
+intents = discord.Intents.all()
+intents.members = True
+client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='!', intents=intents)
 
 thumbnail_url = 'https://thumbnails.roblox.com/v1/users/avatar?userIds='
 url = 'https://users.roblox.com/v1/users/'
@@ -9,57 +14,28 @@ size = '420x420' # Sizes: 720x720, 420x420, 352x552...
 format = 'Png' # Png or Jpeg
 is_circular = 'false' # Determines if the thumbnail is Circular or not
 
-while True:
+@client.command()
+async def robloxinfo(ctx, username):
     try:
-        id = input('Enter ID: ') # Asks for ID
+        id = requests.get(url+'get-by-username?username='+username).json()['id']
         userinfo = requests.get(url+id)
         if userinfo.status_code == 200:
-          data = userinfo.json()
-          username = data['name']
-          display_name = data['displayName']
-          description = data['description']
-          print(f'Username: {username}')
-          print(f'Display name: {display_name}')
-          print(f'description: {description}')
+            data = userinfo.json()
+            username = data['name']
+            display_name = data['displayName']
+            description = data['description']
+            await ctx.send(f'Username: {username}\nDisplay name: {display_name}\nDescription: {description}')
         else:
-          print('Error retrieving user info')
-
-        userhistory = requests.get(url+id+'/username-history')
-        if userhistory.status_code == 200:
-          data = userhistory.json()
-        else:
-          print('Error retrieving username history')
-        print('Past usernames:', end= ' ')
-        for username_data in data['data']:
-            username = username_data['name']
-            print(username, end= ' ')
+            await ctx.send('Error retrieving user info')
 
         userthumbnail = requests.get(thumbnail_url+id+'&size='+size+'&format='+format+'&isCircular='+is_circular)
         if userthumbnail.status_code == 200:
-          data = userthumbnail.json()
-          image = data['data'][0]['imageUrl']
-          print('\n'+image)
+            data = userthumbnail.json()
+            image = data['data'][0]['imageUrl']
+            await ctx.send(image)
         else:
-          print('Error retrieving user thumbnail')
-
+            await ctx.send('Error retrieving user thumbnail')
     except:
-        print(f"An error occurred. Maybe the ID isn't valid. Try again.")
+        await ctx.send('Error: Invalid username or unable to retrieve user info')
 
-    while True:
-        choice = input("Do you want to enter another ID? (y/n): ")
-        if choice.lower() == "y":
-            break
-        elif choice.lower() == "n":
-            print("Goodbye!")
-            sys.exit()
-        else:
-            print("Invalid input. Please enter 'y' or 'n'.")
-
-  
-  
-  
-
-
-
-
-
+client.run("put ur bot token here :D")
