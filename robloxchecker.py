7044844769011220535f6183d0a0eb1e99f79464
nismo <61671297+nismo1337@users.noreply.github.com>
2023-03-28 07:21:58 +0200

    fixed broken code LOL

--- robloxchecker.py
+++ robloxchecker.py
@@ -15,27 +15,20 @@ format = 'Png' # Png or Jpeg
 is_circular = 'false' # Determines if the thumbnail is Circular or not
 
 @client.command()
-async def robloxinfo(ctx, username):
-    try:
-        id = requests.get(url+'get-by-username?username='+username).json()['id']
-        userinfo = requests.get(url+id)
-        if userinfo.status_code == 200:
-            data = userinfo.json()
-            username = data['name']
-            display_name = data['displayName']
-            description = data['description']
-            await ctx.send(f'Username: {username}\nDisplay name: {display_name}\nDescription: {description}')
-        else:
-            await ctx.send('Error retrieving user info')
+async def robloxinfo(ctx, id):
+    userinfo = requests.get(url+id)
+    userthumbnail = requests.get(thumbnail_url+id+'&size='+size+'&format='+format+'&isCircular='+is_circular)
+    if userinfo.status_code == 200 and userthumbnail.status_code == 200:
+        data = userinfo.json()
+        username = data['name']
+        display_name = data['displayName']
+        description = data['description']
+        thumbnail_data = userthumbnail.json()
+        image_url = thumbnail_data['data'][0]['imageUrl']
+        embed = discord.Embed(title=f'Roblox Information for User ID {id}', description=f'Username: {username}\nDisplay name: {display_name}\nDescription: {description}', color=0x00ff00)
+        embed.set_image(url=image_url)
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send('Error retrieving user information')
 
-        userthumbnail = requests.get(thumbnail_url+id+'&size='+size+'&format='+format+'&isCircular='+is_circular)
-        if userthumbnail.status_code == 200:
-            data = userthumbnail.json()
-            image = data['data'][0]['imageUrl']
-            await ctx.send(image)
-        else:
-            await ctx.send('Error retrieving user thumbnail')
-    except:
-        await ctx.send('Error: Invalid username or unable to retrieve user info')
-
-client.run("put ur bot token here :D")
+client.run("put ur bot token here:D")
